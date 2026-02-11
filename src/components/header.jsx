4454f15a3b7161745718452c304b729d0d9d65aa
manoj <manoj>
2026-02-11 15:50:27 +0530

    modified package.json

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,9 @@
-import react from 'react'
-function header()
-    return (
-        <div><h1>Header Component</h1></div>
-    )
-export default header
+import react from "react";
+function Header() {
+  return (
+    <div>
+      <h1>Header Component</h1>
+    </div>
+  );
+}
+export default Header;
